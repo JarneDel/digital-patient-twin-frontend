6dc3b31978c30c94fd57d7bb2f2138e4d1dbe3b3
alessia vinnik <alessia.vinnik@student.howest.be>
2023-05-16 15:03:31 +0200

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,32 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:{
+          500: "#C42828",
+          400: "#FA6262",
+          300: "#FB9191",
+          200: "#F19D99",
+          100: "#E19393",
+          50: "#F8C8CE",
+        },
+        secondary:{
+          500: "#158E87",
+          400: "#44A9A3",
+          300: "#72C4BF",
+          200: "#92D2CE",
+          100: "#CFE7E8",
+        },
+        tertiary:{
+          500: "#927FB8",
+          400: "#AD9FCA",
+          300: "#C8BFDB",
+          200: "#CFC6DF",
+          100: "#D3CCE3",
+        },
+      }
+    },
   },
   plugins: [],
 }
