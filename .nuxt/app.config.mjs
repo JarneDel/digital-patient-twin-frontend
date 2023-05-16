14c6557d50d66d0d20d5319ba18f9e56aa2b4f29
Delarue Jarne <91070860+JarneDel@users.noreply.github.com>
2023-05-16 13:43:19 +0200

    Delete .nuxt directory

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/home/jarne/WebstormProjects/digital-patient-twin-frontend/node_modules/.pnpm/defu@6.1.2/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
