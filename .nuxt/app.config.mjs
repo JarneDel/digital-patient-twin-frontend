095f6818111651e336ebcc6dfcde199a50b5b69d
jarnedel <jarne.delarue@student.howest.be>
2023-05-16 13:40:58 +0200

    change to nuxt

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/home/jarne/WebstormProjects/digital-patient-twin-frontend/node_modules/.pnpm/defu@6.1.2/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
