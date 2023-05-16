3a6c1575cc0b52c38f8627ea3fba04be3df46111
alessia vinnik <alessia.vinnik@student.howest.be>
2023-05-16 16:17:45 +0200

    small changes footer

--- components/template/Footer.vue
+++ components/template/Footer.vue
@@ -3,7 +3,7 @@
 
 
 <template>
-    <footer class="flex justify-center items-center">
-        <div class="text-tertiary-500">MCT 2023</div>
+    <footer class="flex justify-center items-center bg-white">
+        <div class="text-tertiary-500 pb-5 pt-2">MCT 2023</div>
     </footer>
 </template>
