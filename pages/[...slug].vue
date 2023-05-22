65b7a77443281dd30d21a714365fcfbd4b9a03eb
jarnedel <jarne.delarue@student.howest.be>
2023-05-22 09:34:53 +0200

    fix

---
+++ pages/[...slug].vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div>
+    <h1>404 Page not found</h1>
+    <p>{{$route.params.slug}}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
