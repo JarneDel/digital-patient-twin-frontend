b38e6987932adddf058358d6c9b525f479bd37d9
shareeb hashmi <shareeb.hashmi@student.howest.be>
2023-05-23 17:10:26 +0200

    testing slider and styling

--- components/template/slider.vue
+++ components/template/slider.vue
@@ -1,3 +1,20 @@
 <template>
-    
+  <div class="w-64">
+    <label for="">hartslag</label>
+
+    <span class="pl-4 text-sm">0</span>
+    <input
+      type="range"
+      min="0"
+      max="100"
+      name=""
+      id=""
+      class="slider-thumb h-2 w-full appearance-none rounded bg-tertiary-500 focus:outline-none"
+      oninput="this.previousElementSibling.innerText = this.value"
+    />
+    <!-- <div class="-mt-2 flex w-full justify-between">
+      <span class="text-sm text-gray-600">0</span>
+      <span class="text-sm text-gray-600">1000</span>
+    </div> -->
+  </div>
 </template>
