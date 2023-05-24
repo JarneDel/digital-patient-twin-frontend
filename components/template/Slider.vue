7590a9aa8c3c2efc8f987731716b9f2df5e6a0b4
alessia vinnik <alessia.vinnik@student.howest.be>
2023-05-24 16:48:06 +0200

    dubble slider done

--- components/template/Slider.vue
+++ components/template/Slider.vue
@@ -11,12 +11,12 @@
 </script>
 
 <template>
+    <div class="w-52 flex justify-between m-4">
+        <input v-model=value[0] type="text" class="h-10 w-10 text-sm pl-2 border-tertiary-400 border-2 rounded-md focus:border-tertiary-400 focus:ring-0"/>
+        <input v-model=value[1] type="text" class="h-10 w-10 text-sm pl-2 border-tertiary-400 border-2 rounded-md focus:border-tertiary-400 focus:ring-0"/>
+    </div>
     <div class="m-4 w-52">
         <Slider v-model=value :tooltips="false" :min="0" :max="100" class='slider-style' />
-    </div>
-    <div>
-        <input v-model=value[0] type="text" />
-        <input v-model=value[1] type="text" />
     </div>
 </template>
     
