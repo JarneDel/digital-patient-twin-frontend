1b52df8c1fda63bee496afd50f2d0ccb7d332fe6
alessia vinnik <alessia.vinnik@student.howest.be>
2023-05-24 14:07:04 +0200

    last changes

--- components/template/Slider.vue
+++ components/template/Slider.vue
@@ -7,15 +7,25 @@
         },
         data() {
             return {
-                value: 20
+                value_1: [20, 40],
+                value_2: 20
             }
         }
     }
 </script>
 
 <template>
-    <div>
-        <Slider v-model="value" :tooltips="false" :min="0" :max="100"/>
+    <div class="mb-4">
+        <Slider v-model="value_1" :tooltips="false" :min="0" :max="100" :classes="{
+            base: 'w-full h-full relative z-1 bg-gray-300 rounded',
+            connect: 'absolute z-1 top-0 right-0 transform-origin-0 transform-style-flat h-full w-full bg-pink-500 cursor-pointer tap:duration-300 tap:transition-transform disabled:bg-gray-400 disabled:cursor-not-allowed',
+
+        }"/>
+    </div>
+    <div class="mt-4">
+        <Slider v-model="value_2" :tooltips="false" :min="0" :max="100" :classes="{
+            base: 'w-full h-full relative z-1 bg-neutral-300 rounded',
+        }"/>
     </div>
 </template>
     
