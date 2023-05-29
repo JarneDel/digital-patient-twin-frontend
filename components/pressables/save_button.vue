9b6b492947db33af6e2735634fe3a167a71f745f
shareeb hashmi <shareeb.hashmi@student.howest.be>
2023-05-29 21:56:30 +0200

    made a new presseable componenet and imported svg

---
+++ components/pressables/save_button.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button
+      class="rounded bg-primary-500 px-4 py-2 font-bold text-white hover:bg-primary-600"
+      @click="click"
+    >
+      {{ text }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { def } from '@vue/shared'
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['click'])
+
+const click = () => {
+  emit('click')
+}
+</script>
