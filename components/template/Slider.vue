c5abd587e41acb3ef42866c2aeefd3366a23645f
shareeb hashmi <shareeb.hashmi@student.howest.be>
2023-05-30 23:12:03 +0200

    edit page given basic layout

--- components/template/Slider.vue
+++ components/template/Slider.vue
@@ -1,48 +1,58 @@
 <script setup lang="ts">
-    import Slider from '@vueform/slider'
-    import { ref } from "vue";
-    import { AlertType } from '~/interfaces/AlertType'
+import Slider from '@vueform/slider'
+import { ref } from 'vue'
+import { AlertType } from '~/interfaces/AlertType'
 
-    const value = ref([30, 60])
-    const value_1 = ref(10)
+const value = ref([30, 60])
+const value_1 = ref(10)
 
-    defineProps({
-        type: {
-            type: String as PropType<AlertType>,
-            required: true,
-        },
-        min: {
-            type: Number,
-            required: true,
-        },
-        max: {
-            type: Number,
-            required: true,
-        },
-    })
+defineProps({
+  type: {
+    type: String as PropType<AlertType>,
+    required: true,
+  },
+  min: {
+    type: Number,
+    required: true,
+  },
+  max: {
+    type: Number,
+    required: true,
+  },
+})
 
-   
-
-    defineExpose({ value, value_1 })
-
-
+defineExpose({ value, value_1 })
 </script>
 
 <template>
-    <div class="flex justify-center items-end">
-        <div class="mx-3 mb-3 text-base font-medium">{{ type }}</div>
-        <div>
-            <div class="w-52 flex justify-between m-4">
-                <input v-model=value[0] type="text" class="h-8 w-9 text-sm pl-[4px] border-tertiary-400 border-2 rounded-md focus:border-tertiary-400 focus:ring-0 outline-none"/>
-                <input v-model=value[1] type="text" class="h-8 w-9 text-sm pl-[4px] border-tertiary-400 border-2 rounded-md focus:border-tertiary-400 focus:ring-0 outline-none"/>
-            </div>
-            <div class="m-4 w-52">
-                <Slider v-model=value :tooltips="false" :min=min :max=max class='slider-style' />
-            </div>
-        </div>
+  <div class="flex items-end justify-center">
+    <div class="mx-3 mb-3 text-base font-medium">{{ type }}</div>
+    <div>
+      <div class="m-4 flex w-52 justify-between">
+        <input
+          v-model="value[0]"
+          type="text"
+          class="h-8 w-9 rounded-md border-2 border-tertiary-400 pl-[4px] text-sm outline-none focus:border-tertiary-400 focus:ring-0"
+        />
+        <input
+          v-model="value[1]"
+          type="text"
+          class="h-8 w-9 rounded-md border-2 border-tertiary-400 pl-[4px] text-sm outline-none focus:border-tertiary-400 focus:ring-0"
+        />
+      </div>
+      <div class="m-4 w-52">
+        <Slider
+          v-model="value"
+          :tooltips="false"
+          :min="min"
+          :max="max"
+          class="slider-style"
+        />
+      </div>
     </div>
+  </div>
 
-    <!-- <div>
+  <!-- <div>
         <div class="w-52 flex justify-between m-4">
             <input v-model=value_1 type="text" class="h-10 w-11 text-sm pl-[7px] border-tertiary-400 border-2 rounded-md focus:border-tertiary-400 focus:ring-0 outline-none"/>
         </div>
@@ -51,14 +61,13 @@
         </div>
     </div> -->
 </template>
-    
-  
+
 <style src="@vueform/slider/themes/default.css"></style>
 
 <style scoped>
-  .slider-style{
-    --slider-connect-bg: #AD9FCA;
-    --slider-handle-ring-color: #AD9FCA30;
-    --slider-handle-bg: #927FB8;
-  }
+.slider-style {
+  --slider-connect-bg: #ad9fca;
+  --slider-handle-ring-color: #ad9fca30;
+  --slider-handle-bg: #927fb8;
+}
 </style>
--- pages/dokter/[dokterid]/patients/[patientid]/edit.vue
+++ pages/dokter/[dokterid]/patients/[patientid]/edit.vue
@@ -1,11 +1,40 @@
-<script setup lang='ts'>
-
+<script setup lang="ts">
+import { AlertType } from '~/interfaces/AlertType'
 </script>
 
 <template>
+  <div class="align-start m-20 flex justify-between">
+    <PressablesGoback link="Nieuwe Patient aanmaken"></PressablesGoback>
+    <PressablesSaveButton></PressablesSaveButton>
+  </div>
 
+  <forms class="mx-20 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+    <!-- persoonlijke -->
+    <div class="-pl-4">
+      <FormsSelectDevice></FormsSelectDevice>
+      <LazyTextKop2>Meldingen</LazyTextKop2>
+      <pressables-toggle
+        :type="AlertType.temperature"
+        :text="AlertType.temperature"
+      ></pressables-toggle>
+      <div>
+        <template-slider :type="AlertType.breathingRate" :min="0" :max="100" />
+      </div>
+    </div>
+
+    <div class="pl-4">
+      <TextKop2>Persoonlijke gegevens</TextKop2>
+      <FormsTextInput></FormsTextInput>
+      <FormsEmailInput></FormsEmailInput>
+      <FormsBirthDateInput></FormsBirthDateInput>
+    </div>
+
+    <!-- medisch -->
+    <div>
+      <TextKop2>Medische gegevens</TextKop2>
+      <FormsTextInput></FormsTextInput>
+    </div>
+  </forms>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
