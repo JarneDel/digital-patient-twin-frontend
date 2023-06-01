b4f1000abb83924f46ecd6cd95892439c30cc863
alessia vinnik <alessia.vinnik@student.howest.be>
2023-06-01 17:01:45 +0200

    styling done

--- components/chart/table.vue
+++ components/chart/table.vue
@@ -48,54 +48,54 @@ const time = computed(() => {
 <template>
     <div v-if="pending">Loading... {{ pending }}</div>
     <div v-else-if="error">Error: {{ error }}</div>
-  <div class="grid h-[300px] w-[1150px] grid-cols-[2fr_1fr_1fr_1fr_1fr_1fr] grid-rows-6 bg-neutral-600 pt-2 drop-shadow-2xl">
-    <div v-if="time" :key="Math.random()" v-for="timestamp in time" class="-pr-4 flex items-end justify-end bg-primary-50 text-end">
+  <div class="grid items h-[300px] w-[1150px] grid-cols-[2fr_1fr_1fr_1fr_1fr_1fr] grid-rows-6 bg-neutral-600 pt-2 drop-shadow-2xl">
+    <div v-if="time" :key="Math.random()" v-for="timestamp in time" class=" pr-5 flex items-center justify-end bg-primary-50 text-end">
         <div>{{ timestamp }}</div>
     </div>
 
 
     <div class="mr-2 flex items-center justify-between bg-neutral-500 pr-4">
-      <div>zuurstof</div>
+      <div class="ml-8">zuurstof</div>
       <svg-oxygen class="w-8" />
     </div>
-    <div v-if="result" :key="Math.random()" v-for="oxygen in result?.map((item) => item.bloedzuurstof)">
-        <div class="text-center">{{ oxygen.avg }}</div>
+    <div v-if="result" class="flex items-center" :key="Math.random()" v-for="oxygen in result?.map((item) => item.bloedzuurstof)">
+        <div class="pl-7">{{ oxygen.avg.toFixed(1) }}</div>
     </div>
 
 
     <div class="mr-2 flex items-center justify-between bg-neutral-500 pr-4">
-      <div>hartslag</div>
+      <div class="ml-8">hartslag</div>
       <svg-heartrate class="w-8" />
     </div>
-    <div v-if="result" :key="Math.random()" v-for="hartslag in result?.map((item) => item.hartslag)">
-        <div class="text-center">{{ hartslag.avg }}</div>
+    <div v-if="result" class="flex items-center" :key="Math.random()" v-for="hartslag in result?.map((item) => item.hartslag)">
+        <div class="pl-7 ">{{ hartslag.avg.toFixed(0) }}</div>
     </div>
 
 
     <div class="mr-2 flex items-center justify-between bg-neutral-500 pr-4">
-      <div>bloeddruk</div>
+      <div class="ml-8">bloeddruk</div>
       <svg-bloodpressure class="w-10" />
     </div>
-    <div v-if="result" :key="Math.random()" v-for="bloeddruk in result?.map((item) => item.bloeddruk)">
-        <div class="text-center">{{ bloeddruk.diastolic.avg}} / {{ bloeddruk.systolic.avg}}</div>
+    <div v-if="result" class="flex items-center" :key="Math.random()" v-for="bloeddruk in result?.map((item) => item.bloeddruk)">
+        <div class="pl-7 ">{{ bloeddruk.systolic.avg.toFixed(0)}} / {{ bloeddruk.diastolic.avg.toFixed(0)}}</div>
     </div>
 
 
     <div class="mr-2 flex items-center justify-between bg-neutral-500 pr-4">
-      <div>ademfrequentie</div>
+      <div class="ml-8">ademfrequentie</div>
       <svg-ademfrequentie class="w-8" />
     </div>
-    <div v-if="result" :key="Math.random()" v-for="ademFrequentie in result?.map((item) => item.ademFrequentie)">
-        <div class="text-center">{{ ademFrequentie.avg }}</div>
+    <div v-if="result" class="flex items-center" :key="Math.random()" v-for="ademFrequentie in result?.map((item) => item.ademFrequentie)">
+        <div class="pl-7 ">{{ ademFrequentie.avg.toFixed(0) }}</div>
     </div>
 
 
     <div class="mr-2 flex items-center justify-between bg-neutral-500 pr-4">
-      <div>temperatuur</div>
+      <div class="ml-8">temperatuur</div>
       <svg-temperature class="w-8" />
     </div>
-    <div v-if="result" :key="Math.random()" v-for="temperatuur in result?.map((item) => item.temperatuur)">
-        <div class="text-center">{{ temperatuur.avg }}</div>
+    <div v-if="result" class="flex items-center" :key="Math.random()" v-for="temperatuur in result?.map((item) => item.temperatuur)">
+        <div class="pl-7 ">{{ temperatuur.avg.toFixed(1) }}</div>
     </div>
 
   </div>
