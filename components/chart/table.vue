a562369d08f2ef1006419e261439af27a813013f
alessia vinnik <alessia.vinnik@student.howest.be>
2023-06-01 16:34:49 +0200

    data done

--- components/chart/table.vue
+++ components/chart/table.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { timeStamp } from 'console';
 import { IHistoriek } from '~/interfaces/IHistoriek'
 
 const url = 'http://localhost:5012/v1.0/invoke/PatientData/method/history/878c95cf-e82d-40a5-a56c-8790427f1657?range=5'
@@ -25,30 +26,33 @@ const result = computed(() => {
   }
 })
 
+// time, but only hours and minutes
+const time = computed(() => {
+  const mappedTimestamps = [];
+
+  // hours and minutes only
+  if(data.value){
+    mappedTimestamps.push(...data.value.map((item) => new Date(item.timestamp).toLocaleTimeString('nl-BE', { hour: '2-digit', minute: '2-digit' })));
+  }
+  
+  const nextTimestamp = new Date(); // Create a new Date object with the current timestamp
+  mappedTimestamps.push(nextTimestamp.toLocaleTimeString('nl-BE', { hour: '2-digit', minute: '2-digit' }));
+  console.log(mappedTimestamps + " mappedTimestamps");
+  
+  return mappedTimestamps.slice(mappedTimestamps.length - 6, mappedTimestamps.length)
+})
+
+
 </script>
 
 <template>
     <div v-if="pending">Loading... {{ pending }}</div>
     <div v-else-if="error">Error: {{ error }}</div>
   <div class="grid h-[300px] w-[1150px] grid-cols-[2fr_1fr_1fr_1fr_1fr_1fr] grid-rows-6 bg-neutral-600 pt-2 drop-shadow-2xl">
-    <div class="-pr-4 flex items-end justify-end bg-primary-50 text-end">
-        <div>8:00</div>
+    <div v-if="time" :key="Math.random()" v-for="timestamp in time" class="-pr-4 flex items-end justify-end bg-primary-50 text-end">
+        <div>{{ timestamp }}</div>
     </div>
-    <div class="-pr-4 flex items-end justify-end bg-primary-50 text-end">
-        <div>8:20</div>
-    </div>
-    <div class="-pr-4 flex items-end justify-end bg-primary-50 text-end">
-        <div>8:40</div>
-    </div>
-    <div class="-pr-4 flex items-end justify-end bg-primary-50 text-end">
-        <div>9:00</div>
-    </div>
-    <div class="-pr-4 flex items-end justify-end bg-primary-50 text-end">
-        <div>9:20</div>
-    </div>
-    <div class="-pr-4 flex items-end justify-end bg-primary-50 text-end">
-        <div>9:40</div>
-    </div>
+
 
     <div class="mr-2 flex items-center justify-between bg-neutral-500 pr-4">
       <div>zuurstof</div>
@@ -58,6 +62,7 @@ const result = computed(() => {
         <div class="text-center">{{ oxygen.avg }}</div>
     </div>
 
+
     <div class="mr-2 flex items-center justify-between bg-neutral-500 pr-4">
       <div>hartslag</div>
       <svg-heartrate class="w-8" />
@@ -65,6 +70,7 @@ const result = computed(() => {
     <div v-if="result" :key="Math.random()" v-for="hartslag in result?.map((item) => item.hartslag)">
         <div class="text-center">{{ hartslag.avg }}</div>
     </div>
+
 
     <div class="mr-2 flex items-center justify-between bg-neutral-500 pr-4">
       <div>bloeddruk</div>
@@ -74,6 +80,7 @@ const result = computed(() => {
         <div class="text-center">{{ bloeddruk.diastolic.avg}} / {{ bloeddruk.systolic.avg}}</div>
     </div>
 
+
     <div class="mr-2 flex items-center justify-between bg-neutral-500 pr-4">
       <div>ademfrequentie</div>
       <svg-ademfrequentie class="w-8" />
@@ -82,6 +89,7 @@ const result = computed(() => {
         <div class="text-center">{{ ademFrequentie.avg }}</div>
     </div>
 
+
     <div class="mr-2 flex items-center justify-between bg-neutral-500 pr-4">
       <div>temperatuur</div>
       <svg-temperature class="w-8" />
@@ -89,5 +97,6 @@ const result = computed(() => {
     <div v-if="result" :key="Math.random()" v-for="temperatuur in result?.map((item) => item.temperatuur)">
         <div class="text-center">{{ temperatuur.avg }}</div>
     </div>
+
   </div>
 </template>
