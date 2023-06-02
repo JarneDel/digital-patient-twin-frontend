cde168a3d0ab8dd159b1150044499d3cfb95df49
alessia vinnik <alessia.vinnik@student.howest.be>
2023-06-02 11:40:03 +0200

    data fetch done

--- components/patients/realtime.vue
+++ components/patients/realtime.vue
@@ -1,19 +1,24 @@
 <script setup lang='ts'>
-import { IPatientAlgemeen } from '~/interfaces/IPatient'
+import { IPatientAlgemeen, PatientGegevens } from '~/interfaces/IPatient'
 import { LucideLineChart, LucideEdit } from 'lucide-vue-next'
 
 defineProps(
   {
-    'patient': {
-      type: Object as PropType<IPatientAlgemeen>,
-      required: true,
-    },
+    // 'patient': {
+    //   type: Object as PropType<IPatientAlgemeen>,
+    //   required: true,
+    // },
     'type': {
       type: String as PropType<'view' | 'edit'>,
       required: true,
     },
   },
 )
+
+const url =
+  'http://localhost:5012/v1.0/invoke/PatientGegevensService/method/patient/878c95cf-e82d-40a5-a56c-8790427f1657'
+const { error, data, pending } = await useFetch<PatientGegevens>(url)
+
 
 const calculateAge = (date: Date) => {
   // 01/01/2000
@@ -23,23 +28,37 @@ const calculateAge = (date: Date) => {
   return `${day}/${month}/${year}`
 }
 
+const result = computed<IPatientAlgemeen[]>(() => {
+  const lijst: IPatientAlgemeen[] = []
+  if (data.value?.algemeen) {
+    lijst.push(data.value.algemeen)
+  }
+
+  return lijst
+})
 // fetching function for the realtime data or websocket
 
 </script>
 
 <template>
-  <div class='flex flex-row justify-between'>
+  <div v-if="pending">Loading... {{ pending }}</div>
+  <div v-else-if="error">Error: {{ error }}</div>
+  <div class='flex flex-row justify-between bg-neutral-500 p-8 rounded-lg'>
     <div class='flex flex-row justify-start content-center gap-2'>
-      <div>
-        {{ patient.voornaam }}
-        {{ patient.achternaam }}
+      <div
+      v-if="result"
+      :key="Math.random()"
+      v-for="naam in result"
+      >
+      {{ naam.voornaam }}
+        <!-- {{ naam.achternaam }} -->
       </div>
-      <div>
-        {{ calculateAge(patient.geboortedatum) }}
+      <!-- <div v-for="naam in result">
+        {{ calculateAge(naam.geboortedatum) }}
       </div>
-      <div>
-        {{ patient.geslacht }}
-      </div>
+      <div v-for="naam in result">
+        {{ naam.geslacht }}
+      </div> -->
     </div>
     <div class='flex flex-row flex-1 justify-end content-center'>
       <button v-if='type === "view"'>
