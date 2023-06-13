e0e4b84611387338e59eb014a3a82c98d857f16d
alessia vinnik <alessia.vinnik@student.howest.be>
2023-06-13 10:16:39 +0200

    doktername and email

--- pages/dokter/account/index.vue
+++ pages/dokter/account/index.vue
@@ -8,12 +8,27 @@ useHead({
     },
   ],
 })
+
+function divideFullName(fullName: string): [string, string] {
+  const names = fullName.split(' ');
+  const firstName = names[0];
+  const lastName = names[names.length - 1];
+
+  return [firstName, lastName];
+}
+
+const fullName = useUser().value?.idTokenClaims?.name?.toString() ?? '';
+
+const [firstName, lastName] = divideFullName(fullName);
+console.log('First Name:', firstName);
+console.log('Last Name:', lastName); 
 </script>
 
 <template>
   <div>hallo</div>
-  <div>{{ useUser() }}</div>
   <div>{{ useUser().value?.idTokenClaims?.name }}</div>
+  <div>{{ divideFullName(fullName)[0] }}</div>
+  <div>{{ divideFullName(fullName)[1] }}</div>
   <div>{{ useUser().value?.idTokenClaims?.preferred_username }}</div>
 </template>
 
