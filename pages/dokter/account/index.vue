fba0c14a354c8811599f58785cbacb72f9f2c698
alessia vinnik <alessia.vinnik@student.howest.be>
2023-06-13 11:38:44 +0200

    basis card

--- pages/dokter/account/index.vue
+++ pages/dokter/account/index.vue
@@ -29,11 +29,13 @@ console.log('Last Name:', lastName)
     <h2 v-if="fullName !== null" class="mx-8 mb-8 mt-6 text-3xl font-semibold">
       Mijn gegevens
     </h2>
-    <div class="bg-secondary-300 rounded-lg h-auto py-12">
-      <div class="m-5 flex flex-col items-center justify-between md:flex-row">
-      <div
-        class="mx-5 grid grid-cols-1 gap-4 md:grid-cols-2 lg:mx-20"
-      >
+    <div class="mx-8 h-auto w-fit rounded-lg bg-secondary-300 py-8 px-6 shadow-normal">
+      <img
+      class='w-32 h-32 mx-auto rounded-full object-cover'
+      src='/Images/random_profile.jpg'
+      alt='doctor'
+      />
+      <div class="mx-8 my-2 flex flex-col items-center justify-center md:flex-row">
         <div class="lg:col-span-1">
           <TextKop2 class="my-5">Persoonlijke informatie</TextKop2>
           <div class="flex flex-col gap-5">
@@ -46,15 +48,16 @@ console.log('Last Name:', lastName)
               <div>{{ divideFullName(fullName)[1] }}</div>
             </div>
           </div>
-          
+
           <TextKop2 class="my-5">Contact informatie</TextKop2>
           <div class="flex items-center">
             <h1 class="mr-3 font-medium capitalize">email:</h1>
-            <div>{{ useUser().value?.idTokenClaims?.preferred_username }}</div>
+            <div>
+              {{ useUser().value?.idTokenClaims?.preferred_username }}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   </div>
 </template>
