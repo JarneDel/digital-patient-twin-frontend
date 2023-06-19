0ddfd1d3c4d6726485199ca92e21aaf0e834ff8f
alessia vinnik <alessia.vinnik@student.howest.be>
2023-06-19 15:25:20 +0200

    margins done

--- pages/dokter/patienten/[patientid]/index.vue
+++ pages/dokter/patienten/[patientid]/index.vue
@@ -21,8 +21,8 @@ useTitle('Vitalen ' + patient.value?.algemeen.voornaam + " " + patient.value?.al
 </script>
 
 <template>
-  <div class="mx-auto my-12 max-w-7xl">
-    <text-kop2 v-if='patient' class='mx-auto mb-2 mt-5'>Vitalen {{ patient.algemeen.voornaam }} {{ patient.algemeen.naam}}</text-kop2>
+  <div class="md:mx-20 my-12 mx-8">
+    <text-kop2 v-if='patient' class='mt-5 mb-2 text-3xl font-semibold text-neutral-800'>Vitalen {{ patient.algemeen.voornaam }} {{ patient.algemeen.naam}}</text-kop2>
     <client-only>
       <grafieken :for='id' />
     </client-only>
