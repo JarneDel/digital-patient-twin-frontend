d8a9de20ffd31d4abd3a13328b160597f72dcd02
alessia vinnik <alessia.vinnik@student.howest.be>
2023-06-19 11:03:57 +0200

    404

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -17,11 +17,11 @@ useHead({
     class="flex min-h-screen flex-col-reverse items-center justify-center text-center font-mono text-gray-600 dark:text-gray-400"
   >
     <div>
-      <h2 class="text-2xl font-bold md:text-6xl">404: Page not found</h2>
-      <p class="mt-4 text-lg">
+      <h2 class="text-2xl text-neutral-200 font-bold md:text-6xl">404: Page not found</h2>
+      <p class="mt-4 text-lg text-neutral-200">
         Sorry, but the page you were trying to view does not exist.
       </p>
-      <nuxt-link to="/" class="mt-4 max-w-fit text-lg">
+      <nuxt-link to="/" class="mt-4 max-w-fit text-neutral-200 text-lg">
         <div class="row flex gap-2">
           <LucideArrowLeft />
           <span> Go back home </span>
