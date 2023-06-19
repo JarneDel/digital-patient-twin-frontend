84e2f0911fde4f0db1c9522e9b242de630efefbc
alessia vinnik <alessia.vinnik@student.howest.be>
2023-06-19 11:50:52 +0200

    go back done

--- components/pressables/goback.vue
+++ components/pressables/goback.vue
@@ -20,7 +20,7 @@ defineProps({
 
 <template>
   <div class="flex items-center p-5">
-    <NuxtLink class='cursor-pointer mx-2 inline-flex justify-center rounded-lg border border-transparent bg-secondary-100 text-sm font-medium ring-2 ring-transparent hover:bg-secondary-200/40 outline-none focus-visible:ring-secondary-400/80'
+    <NuxtLink tabindex="0" class='cursor-pointer mx-2 inline-flex justify-center rounded-lg border border-transparent bg-secondary-100 text-sm font-medium ring-2 ring-transparent hover:bg-secondary-200/40 outline-none focus-visible:ring-secondary-400/80'
               @click='$router.go(-1)'>
       <ArrowLeft :size="28" />
     </NuxtLink>
