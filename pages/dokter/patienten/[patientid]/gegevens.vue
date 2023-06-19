a2142202d7a46a7d58db178ebaf0c52c2d1f01d1
alessia vinnik <alessia.vinnik@student.howest.be>
2023-06-19 13:53:57 +0200

    button aangepast

--- pages/dokter/patienten/[patientid]/gegevens.vue
+++ pages/dokter/patienten/[patientid]/gegevens.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { Address, IPatientAlgemeen, Medisch, PatientGegevens } from '~/interfaces/IPatient'
+import { PatientGegevens } from '~/interfaces/IPatient'
+import { LucideEdit3 } from 'lucide-vue-next'
 
-useTitle("Gegevens patiënt", "Patiënt detailpagina.")
+useTitle('Gegevens patiënt', 'Patiënt detailpagina.')
 const routeID = useRoute().params.patientid as string
 
 //id dynamicaaly
@@ -9,23 +10,26 @@ const id = ref(routeID)
 
 const url = `https://patientgegevens--hml08fh.blackdune-2fd1ec46.northeurope.azurecontainerapps.io/patient/${id.value}`
 
-// const url = `/dokter/${user.value?.localAccountId}/patients/${patientId}`
-
 const { error, data, pending } = useFetch<PatientGegevens>(url)
 
-const {convertDateStringToLocaleString} = UseDateConverter()
+const { convertDateStringToLocaleString } = UseDateConverter()
 </script>
 
 <template>
-  <div class="m-5 flex flex-col items-center justify-between md:flex-row lg:mx-20 max-w-[74rem]">
+  <div
+    class="m-5 flex max-w-[74rem] flex-col items-center justify-between md:flex-row lg:mx-20"
+  >
     <PressablesGoback
       link_path="/dokter/patienten"
       link_name="patient gegevens"
     ></PressablesGoback>
 
-    <NuxtLink :to="`/dokter/patienten/${id}/edit`">
-      <PressablesEdit></PressablesEdit>
-    </NuxtLink>
+    <button
+      class="duration-400 m-2 w-10 min-w-max rounded-lg border border-transparent bg-tertiary-100/30 p-5 outline-none ring-2 ring-tertiary-300 transition-all hover:cursor-pointer hover:bg-tertiary-200/40 focus-visible:ring-tertiary-500"
+      @click="() => $router.push(`/dokter/patienten/${id}/edit`)"
+    >
+      <LucideEdit3 class="text-tertiary-600 h-8 w-8" />
+    </button>
   </div>
 
   <div
@@ -35,17 +39,12 @@ const {convertDateStringToLocaleString} = UseDateConverter()
     <!-- profiel -->
     <div class="mx-auto lg:col-span-1">
       <div class="flex text-lg font-semibold">
-        <p
-          class="mx-auto text-center text-2xl font-semibold"
-        >
+        <p class="mx-auto text-center text-2xl font-semibold">
           {{ data.algemeen.voornaam }} {{ data.algemeen.naam }}
         </p>
       </div>
       <div>
-        <p
- 
-          class="mx-auto text-center text-sm font-normal text-neutral-100"
-        >
+        <p class="mx-auto text-center text-sm font-normal text-neutral-100">
           {{ data.contact.email }}<br />
           {{ convertDateStringToLocaleString(data.algemeen.geboorteDatum) }}
         </p>
@@ -54,10 +53,7 @@ const {convertDateStringToLocaleString} = UseDateConverter()
       <!-- meldingen -->
       <div class="relative">
         <svg-fitbit class="mx-auto"></svg-fitbit>
-        <p
-          class="absolute inset-0 top-12 text-center text-sm font-semibold"
- 
-        >
+        <p class="absolute inset-0 top-12 text-center text-sm font-semibold">
           {{ '#' + data.deviceId }}
           <!-- #1 -->
         </p>
@@ -69,20 +65,20 @@ const {convertDateStringToLocaleString} = UseDateConverter()
       <TextKop2 class="my-5">Persoonlijke informatie</TextKop2>
       <div class="form-item">
         <div>
-          <h4 >geslacht</h4>
-          <p >
+          <h4>geslacht</h4>
+          <p>
             {{ data.algemeen.geslacht }}
           </p>
         </div>
         <div>
-          <h4 >Geboortedatum:</h4>
-          <p >
+          <h4>Geboortedatum:</h4>
+          <p>
             {{ convertDateStringToLocaleString(data.algemeen.geboorteDatum) }}
           </p>
         </div>
-        <div >
-          <h4 >Geboorteland:</h4>
-          <p >
+        <div>
+          <h4>Geboorteland:</h4>
+          <p>
             {{ data.algemeen.geboorteland }}
           </p>
         </div>
@@ -91,28 +87,28 @@ const {convertDateStringToLocaleString} = UseDateConverter()
       <!-- adres -->
       <TextKop2 class="my-5">Adres informatie</TextKop2>
       <div class="form-item">
-        <div >
-          <h4 >Straatnaam:</h4>
-          <p >
+        <div>
+          <h4>Straatnaam:</h4>
+          <p>
             {{ data.adres.straat }}
           </p>
         </div>
         <div>
-          <h4 >Huisnummer:</h4>
-          <p >
+          <h4>Huisnummer:</h4>
+          <p>
             {{ data.adres.nr }}
           </p>
         </div>
-        <div >
-          <h4 >Postcode:</h4>
-          <p >
+        <div>
+          <h4>Postcode:</h4>
+          <p>
             {{ data.adres.postcode }}
           </p>
         </div>
-        <div >
-          <h4 >Gemeente:</h4>
+        <div>
+          <h4>Gemeente:</h4>
           <!-- <p>{{ patientAdres.gemeente }}</p> -->
-          <p >
+          <p>
             {{ data.adres.gemeente }}
           </p>
         </div>
@@ -124,20 +120,20 @@ const {convertDateStringToLocaleString} = UseDateConverter()
       <TextKop2 class="my-5">Medische informatie</TextKop2>
       <div class="form-item">
         <div>
-          <h4 >Lengte:</h4>
-          <p >
+          <h4>Lengte:</h4>
+          <p>
             {{ data.medisch.lengte + ' cm' }}
           </p>
         </div>
-        <div >
-          <h4 >Gewicht:</h4>
-          <p >
+        <div>
+          <h4>Gewicht:</h4>
+          <p>
             {{ data.medisch.gewicht + ' kg' }}
           </p>
         </div>
-        <div >
-          <h4 >Bloedgroep:</h4>
-          <p >
+        <div>
+          <h4>Bloedgroep:</h4>
+          <p>
             {{ data.medisch.bloedgroep }}
           </p>
         </div>
@@ -172,5 +168,4 @@ const {convertDateStringToLocaleString} = UseDateConverter()
   text-transform: capitalize;
   font-weight: 500;
 }
-
 </style>
